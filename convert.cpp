b1581fa46b7e9db871ecc6135bce3b1cf5dafaeb
Ahmad1704 <101527171+Ahmad1704@users.noreply.github.com>
2024-12-02 11:12:56 +0200

    Update convert.cpp

---
+++ convert.cpp
@@ -0,0 +1,14 @@
+#include "convert.h"
+
+float convert::PIXELS_PER_METER = 4.0f;
+
+sf::Vector2f convert::metersToPixels(const b2Vec2& meters)
+{
+    return sf::Vector2f(meters.x * PIXELS_PER_METER, meters.y * PIXELS_PER_METER);
+}
+
+
+b2Vec2 convert::pixelsToMeters(const sf::Vector2i& pixelPos)
+{
+    return b2Vec2(pixelPos.x / PIXELS_PER_METER, pixelPos.y / PIXELS_PER_METER);
+}
